ef6e789c5b59c3ed5c398d194fbee69bf7189a66
PedroTeixeiraa <pedroteixeiraalves007@gmail.com>
2023-07-06 22:10:27 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
